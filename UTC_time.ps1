01fd3a14d75862580eb3158dd950a1fc627aa64a
Sergi Castro <sergicastro2001@gmail.com>
2020-11-16 21:14:37 +0100

    Added powershell script

---
+++ UTC_time.ps1
@@ -0,0 +1,3 @@
+﻿New-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation" -Name "RealTimeIsUniversal" -Value 1 -PropertyType "QWord"
+cmd.exe /c "sc config w32time start= disabled"
+Restart-Computer -Confirm
